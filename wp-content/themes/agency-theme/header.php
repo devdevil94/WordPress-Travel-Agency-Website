a779ae5cc8c6c1e1353c86231a9c118796c4489b
Saud <saudd2gh@gmail.com>
2018-09-26 09:21:54 +0800

    navbar modified

--- wp-content/themes/agency-theme/header.php
+++ wp-content/themes/agency-theme/header.php
@@ -8,38 +8,29 @@
 
 	<body <?php body_class(); ?>>
 		<div class="header-nightsky">
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <a class="navbar-brand" href="#">Omanova</a>
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">HOME</a></li>
-                        <li class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">NEWS <span class="caret"></span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">FEATURES <span class="caret"></span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">DEMOS<span class="caret"></span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="hero">
-            <h1>This is a Bootstrap header</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
-            <div class="btn btn-primary">Learn more about us</div>
-            <div class="btn btn-primary">Contact us</div>
-        </div>
-    </div>
+	        <nav class="navbar navbar-default">
+	            <a class="navbar-brand" href="#">Omanova</a>
+	           
+	            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myNavbar">
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	            </button>
+
+	            <div class="collapse nav-collapse" id="myNavbar">
+	                <div class="navbar-nav">
+	                    <a class="nav-item nav-link active" href="#">HOME</a>
+	                    <a class="nav-item nav-link" href="#">NEWS <span class="caret"></span></a>
+	                    <a class="nav-item nav-link"" href="#">DEMOS<span class="caret"></span></a>
+	            	</div>
+	            </div>
+	        </nav>
+	        <div class="hero">
+	            <h1>This is a Bootstrap header</h1>
+	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
+	            <div class="btn btn-primary">Learn more about us</div>
+	            <div class="btn btn-primary">Contact us</div>
+	        </div>
+    	</div>
 	</body>
 </html>

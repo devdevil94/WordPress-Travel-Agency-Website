abe97413235f371c463015e0e8232615220b8c55
Saud <saudd2gh@gmail.com>
2018-09-26 08:49:04 +0800

    basic header template added

--- wp-content/themes/agency-theme/functions.php
+++ wp-content/themes/agency-theme/functions.php
@@ -1,11 +1,16 @@
 
 <?php 
-	function load_bootstrap_files(){
-		wp_enqueue_style('bootstrap-css', '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
-		wp_enqueue_style('main_styles', get_stylesheet_uri());
-
-		wp_enqueue_script('bootstrap-js', '//code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+	function bootstrap_files(){
+		wp_enqueue_style('bootstrap-css','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+		wp_enqueue_script('bootstrap-js','//code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ), true);
 	}
 
-	add_action('wp_enqueue_scripts', 'load_bootstrap_files');
+	function custom_styles(){
+		wp_enqueue_style('main-styles', get_stylesheet_uri());
+		wp_enqueue_style('header-style',get_template_directory_uri().'/css/header_style.css',array(),null,'all');
+		wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Montserrat');
+	}
+
+	add_action('wp_enqueue_scripts', 'bootstrap_files');
+	add_action('wp_enqueue_scripts', 'custom_styles');
 ?>

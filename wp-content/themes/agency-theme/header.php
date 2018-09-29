5b8ea4d56af4f0d9a531d2abf8ac8393a2c4393d
Saud <saudd2gh@gmail.com>
2018-09-29 11:41:47 +0800

    minor fixes

--- wp-content/themes/agency-theme/header.php
+++ wp-content/themes/agency-theme/header.php
@@ -8,14 +8,14 @@
 
 	<body <?php body_class(); ?>>
 		<div class="header-nightsky">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<nav class="navbar navbar-expand-lg navbar-default">
 	            <a class="navbar-brand" href="#">Omanova</a>
 
 			  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			    	<span class="navbar-toggler-icon"></span>
 			  	</button>
 			  	
-		  		<div class="collapse navbar-collapse navbar-nav ml-auto justify-content-end" id="navbarNavAltMarkup">
+		  		<div class="collapse navbar-collapse nav navbar-nav ml-auto justify-content-end" id="navbarNavAltMarkup">
 			      	<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
 			    	<a class="nav-item nav-link" href="#">Features</a>
 			      	<a class="nav-item nav-link" href="#">Pricing</a>

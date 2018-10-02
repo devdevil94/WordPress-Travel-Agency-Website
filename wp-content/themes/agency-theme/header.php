b2a9f15aad9e14479766298b13ae0943ee0e94b6
Saud <saudd2gh@gmail.com>
2018-10-02 11:03:50 +0800

    navbar centered

--- wp-content/themes/agency-theme/header.php
+++ wp-content/themes/agency-theme/header.php
@@ -7,25 +7,30 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<header class="header-nightsky">
-        	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			  <a class="navbar-brand" href="#">Navbar</a>
-			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			    <span class="navbar-toggler-icon"></span>
-			  </button>
-			  <div class="collapse navbar-collapse" id="navbarNav">
-			    <div class="navbar-nav">
-			      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-			      <a class="nav-item nav-link" href="#">Features</a>
-			      <a class="nav-item nav-link" href="#">Pricing</a>
-			      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-			    </div>
-			  </div>
-			</nav>
-	        <div class="hero">
+		<header class="header-nightsky d-flex flex-column justify-content-end">
+			<a class="navbar-brand" href="#">Navbar</a>
+
+			<div class="hero">
 	            <h1>This is a Bootstrap header</h1>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
 	            <div class="btn btn-primary">Learn more about us</div>
 	            <div class="btn btn-primary">Contact us</div>
 	        </div>
+
+        	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="nav navbar-collapse collapse justify-content-center" id="navbarNav">
+			    <div class="navbar-nav mx-auto">
+			      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+			      <a class="nav-item nav-link" href="#">Packages</a>
+			      <a class="nav-item nav-link" href="#">Attractions</a>
+			      <a class="nav-item nav-link" href="#">About Us</a>
+			      <a class="nav-item nav-link" href="#">Contact</a>
+			    </div>
+			  </div>
+			</nav>
+	        
     	</header>

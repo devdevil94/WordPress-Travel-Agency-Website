3e07f3b2f8df02a622b6240a7c77136717f31df3
Saud <saudd2gh@gmail.com>
2018-10-02 11:03:04 +0800

    header redesigned

--- wp-content/themes/agency-theme/footer.php
+++ wp-content/themes/agency-theme/footer.php
@@ -1,1 +1,9 @@
-<h1>Footer</h1>
+
+
+<footer>
+	<h1>Footer</h1>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/agency-theme/header.php
+++ wp-content/themes/agency-theme/header.php
@@ -7,28 +7,25 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div class="header-nightsky">
-			<nav class="navbar navbar-expand-lg navbar-default">
-	            <a class="navbar-brand" href="#">Omanova</a>
-
-			  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			    	<span class="navbar-toggler-icon"></span>
-			  	</button>
-			  	
-		  		<div class="collapse navbar-collapse nav navbar-nav ml-auto justify-content-end" id="navbarNavAltMarkup">
-			      	<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-			    	<a class="nav-item nav-link" href="#">Features</a>
-			      	<a class="nav-item nav-link" href="#">Pricing</a>
-			      	<a class="nav-item nav-link" href="#">Disabled</a>
-			  	</div>
-        	</nav>
-
+		<header class="header-nightsky">
+        	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <a class="navbar-brand" href="#">Navbar</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarNav">
+			    <div class="navbar-nav">
+			      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+			      <a class="nav-item nav-link" href="#">Features</a>
+			      <a class="nav-item nav-link" href="#">Pricing</a>
+			      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+			    </div>
+			  </div>
+			</nav>
 	        <div class="hero">
 	            <h1>This is a Bootstrap header</h1>
 	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
 	            <div class="btn btn-primary">Learn more about us</div>
 	            <div class="btn btn-primary">Contact us</div>
 	        </div>
-		</div>
-	</body>
-</html>
+    	</header>

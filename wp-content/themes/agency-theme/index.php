afac6dd0d6074fb7e8bda195071697b14fb2e96f
Saud <saudd2gh@gmail.com>
2018-10-04 20:41:06 +0800

    minor fixes

--- wp-content/themes/agency-theme/index.php
+++ wp-content/themes/agency-theme/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
-
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<hr class="w-75 my-5">
+<div id="carouselExampleIndicators" class="carousel slide mx-auto" style="width: 50%" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -8,13 +8,25 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="<?php echo get_theme_file_uri('img/41117494141_a2809ae23e_o.jpg') ?>" alt="First slide">
+      	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/41117494141_a2809ae23e_o.jpg'); ?>" alt="First slide">
+    	<div class="carousel-caption d-none d-md-block">
+	    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
+	    	<button type="button" class="btn btn-info">Read More</button>
+	  	</div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+      	<img class="d-block w-100" src="<?php echo get_theme_file_uri('39390331741_56abc913e4_o.jpg'); ?>" alt="Second slide">
+      	<div class="carousel-caption d-none d-md-block">
+	    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
+	    	<button type="button" class="btn btn-info">Reore</button>
+	  	</div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+      	<img class="d-block w-100" src="<?php echo get_theme_file_uri('39394819962_616c85c251_o.jpg'); ?>" alt="Third slide">
+      	<div class="carousel-caption d-none d-md-block">
+	    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
+	    	<button type="button" class="btn btn-info">Rade</button>
+	  	</div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">

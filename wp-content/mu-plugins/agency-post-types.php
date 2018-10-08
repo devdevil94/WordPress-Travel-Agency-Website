7b10d051bc1d9a50e5a4505a7f16ef191a7907c1
Saud <saudd2gh@gmail.com>
2018-10-08 19:01:37 +0800

    package post type added

---
+++ wp-content/mu-plugins/agency-post-types.php
@@ -0,0 +1,21 @@
+<?php 
+
+	function agency_post_types(){
+		//Package Post Type
+		register_post_type('Package', array( 
+			'has_archive' => true,
+			'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
+			'rewrite' => array('slug' => 'packages'),
+			'public' => true,
+			'labels' => array(
+						'name' => 'Packages',
+						'add_new_item' => 'Add New Package',
+						'edit_item' => 'Edit Package',
+						'all_items' => 'All Packages',
+						'singular_name' => 'Package'),
+			'menu_icon' => 'dashicons-calendar'
+		));
+	}
+
+	add_action( 'init', 'agency_post_types');
+?>

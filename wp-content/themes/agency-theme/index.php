f7711414bedb0c9759f8046f911cc6f1a81584c6
Saud <saudd2gh@gmail.com>
2018-10-08 18:51:17 +0800

    package cards added

--- wp-content/themes/agency-theme/index.php
+++ wp-content/themes/agency-theme/index.php
@@ -2,32 +2,34 @@
 <hr class="w-75 my-5">
 
 <div class="container">
-	<div id="carouselExampleIndicators" class="carousel slide mx-auto" style="width: 60%" data-ride="carousel">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 		<ol class="carousel-indicators">
 			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
 			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 		</ol>
+
 		<div class="carousel-inner">
-		<div class="carousel-item active">
-		  	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/41117494141_a2809ae23e_o.jpg'); ?>" alt="First slide">
-			<div class="carousel-caption d-none d-md-block">
-		    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
-		  	</div>
+			<div class="carousel-item active">
+			  	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/41117494141_a2809ae23e_o.jpg'); ?>" alt="First slide">
+				<div class="carousel-caption d-none d-md-block">
+			    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
+			  	</div>
+			</div>
+			<div class="carousel-item">
+			  	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/39390331741_56abc913e4_o.jpg'); ?>" alt="Second slide">
+			  	<div class="carousel-caption d-none d-md-block">
+			    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
+			  	</div>
+			</div>
+			<div class="carousel-item">
+			  	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/39394819962_616c85c251_o.jpg'); ?>" alt="Third slide">
+			  	<div class="carousel-caption d-none d-md-block">
+			    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
+			  	</div>
+			</div>
 		</div>
-		<div class="carousel-item">
-		  	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/39390331741_56abc913e4_o.jpg'); ?>" alt="Second slide">
-		  	<div class="carousel-caption d-none d-md-block">
-		    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
-		  	</div>
-		</div>
-		<div class="carousel-item">
-		  	<img class="d-block w-100" src="<?php echo get_theme_file_uri('img/39394819962_616c85c251_o.jpg'); ?>" alt="Third slide">
-		  	<div class="carousel-caption d-none d-md-block">
-		    	<h5 class="text-dark">Sultan Qaboos Mosque</h5>
-		  	</div>
-		</div>
-		</div>
+
 		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
@@ -39,6 +41,36 @@
 	</div>
 </div>
 
+<hr class="w-75 my-5">
+
+<div class="container d-flex justify-content-around">
+	<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="<?php echo get_theme_file_uri('img/39394819962_616c85c251_o.jpg'); ?>" alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+
+	<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="<?php echo get_theme_file_uri('img/39394819962_616c85c251_o.jpg'); ?>" alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+
+	<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="<?php echo get_theme_file_uri('img/39394819962_616c85c251_o.jpg'); ?>" alt="Card image cap">
+		<div class="card-body">
+			<h5 class="card-title">Card title</h5>
+			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+</div>
 
 
 <?php get_footer(); ?>

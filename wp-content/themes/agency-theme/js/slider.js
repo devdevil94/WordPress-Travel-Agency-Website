4d8a4fb7ab098370fcdfd1228ae86e3e7b467835
Saud <saudd2gh@gmail.com>
2018-11-05 23:30:06 +0400

    minor fixes

--- wp-content/themes/agency-theme/js/slider.js
+++ wp-content/themes/agency-theme/js/slider.js
@@ -11,14 +11,18 @@ function showTestimonial(n) {
   var i;
   var testimonials = document.getElementsByClassName("testimonial");
   var dots = document.getElementsByClassName("t-dot");
-  if (n > testimonials.length) {testimonialIndex = 1} 
-  if (n < 1) {testimonialIndex = testimonials.length}
+
+  if (n > testimonials.length) {testimonialIndex = 1;} 
+  if (n < 1) {testimonialIndex = testimonials.length;}
+
   for (i = 0; i < testimonials.length; i++) {
     testimonials[i].style.display = "none"; 
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  
   testimonials[testimonialIndex-1].style.display = "block"; 
   dots[testimonialIndex-1].className += " active";
 }
